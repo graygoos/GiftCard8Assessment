db1fd83d3a7e82e444503fde5ab2aa305b0d0c8b
Femi Aliu <femialiu713@gmail.com>
2025-08-06 13:13:01 +0100

    Initial commit with .gitignore and project setup files

---
+++ GiftCard8Assessment/GiftCard8AssessmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  GiftCard8AssessmentApp.swift
+//  GiftCard8Assessment
+//
+//  Created by Femi Aliu on 06/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct GiftCard8AssessmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
